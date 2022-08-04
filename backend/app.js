964fc7b657d76c5c0ea5db5870c80a47273c5266
NivinClinton <nivinclint@gmail.com>
2022-08-04 09:55:25 +0530

    code improvement

--- backend/app.js
+++ backend/app.js
@@ -11,13 +11,10 @@ app.use(express.json());
 
 
 app.use("/api/book", bookRouter);
-// app.use('/', bookRouter)
-
-// mongoose.connect("mongodb+srv://nivinclinton:Vinvin2025@cluster0.lkwbno6.mongodb.net/?retryWrites=true&w=majority").then(()=>app.listen(8000))
-// .then(()=>console.log("database connected")).catch((err)=>console.log(err))
 
 mongoose.connect("mongodb+srv://nivinclinton:Vinvin2025@cluster0.lkwbno6.mongodb.net/?retryWrites=true&w=majority").then(() => {
     console.log('Database Connected')
+    app.listen(3000, () => {
+        console.log('Server Connected');
+    })
 }).catch((error) => console.log('THERE IS AN ERROR' + error))
-
-app.listen(3000)
