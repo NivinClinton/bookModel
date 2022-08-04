50e52757f17973a55ec4642afdbd3a9b0f227c22
NivinClinton <nivinclint@gmail.com>
2022-08-04 10:47:19 +0530

    env file excluded

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 import express from "express";
 import bookRouter from "./routes/bookRoutes.js";
+import dotenv from 'dotenv'
 
 
 const app = express();
+
+dotenv.config()
 
 app.use(express.json());
 // app.use(cors());
@@ -12,9 +15,7 @@ app.use(express.json());
 
 app.use("/api/book", bookRouter);
 
-mongoose.connect("mongodb+srv://nivinclinton:Vinvin2025@cluster0.lkwbno6.mongodb.net/?retryWrites=true&w=majority").then(() => {
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.lkwbno6.mongodb.net/?retryWrites=true&w=majority`).then(() => {
     console.log('Database Connected')
-    app.listen(3000, () => {
-        console.log('Server Connected');
-    })
+    app.listen(3000, () => console.log('Server Connected'))
 }).catch((error) => console.log('THERE IS AN ERROR' + error))
